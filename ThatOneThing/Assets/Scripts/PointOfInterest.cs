80a8b6a9eb64ee25d722b0f9f26ae1fd371c4431
Justin Brûlotte <Justin2997@users.noreply.github.com>
2019-01-26 11:42:03 -0500

    Charles qui merge des truques
    * First draft scripts
    
    * Well Being stuff - v1
    
    slider + meter dans le UI
    
    * cleanup machins
    
    * MODIF GITignore
    
    * First draft scripts
    
    * Well Being stuff - v1
    
    slider + meter dans le UI
    
    * MODIF GITignore

---
+++ ThatOneThing/Assets/Scripts/PointOfInterest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour
+{
+    [SerializeField]
+    public float deltaWB { get; protected set; } // delta du bien-être instantané
+
+    [SerializeField]
+    public float optimumWB { get; protected set; }  // point auquel le gain de WB est maximum; dépassé ce point, le gain diminue
+
+    [SerializeField]
+    public float deltaWBRate { get; protected set; }  // delta de la dérivée du bien-être
+
+    [SerializeField]
+    public float timeCost { get; protected set; }
+
+    //[SerializeField]
+    //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
