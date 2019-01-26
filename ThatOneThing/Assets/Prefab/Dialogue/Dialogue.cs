da29c225b41367f0424f19521622673210210ff7
PlasticPack <plasticpack@hotmail.ca>
2019-01-26 13:36:22 -0500

    Addded DialogueManager and Dialogue Data

---
+++ ThatOneThing/Assets/Prefab/Dialogue/Dialogue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Dialogue : MonoBehaviour
+{
+    [TextArea(1, 10)]
+    public string[] Sentences;
+    public string DialogueName;
+}
