dd30c7648f574a571a37d3201aeb3c44c2d1079f
Justin Brûlotte <Justin2997@users.noreply.github.com>
2019-01-26 13:27:38 -0500

    Adding Moving Player and Social and impact and other
    * Added some sprites and tilepalettes
    
    * First draft scripts
    
    * debut de la map
    
    * Well Being stuff - v1
    
    slider + meter dans le UI
    
    * cleanup machins
    
    * MODIF GITignore
    
    * First draft scripts
    
    * Well Being stuff - v1
    
    slider + meter dans le UI
    
    * MODIF GITignore
    
    * social stuff v# + ignore .info
    
    * Adding moving
    
    * Prefabs
    
    * fuck library

---
+++ ThatOneThing/Assets/Scripts/Social.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Social : MonoBehaviour
+{
+    public Dictionary<Social, float> relations; // -1 à 1
+    public Dictionary<string, float> opinions; // -1 à 1
+
+    // Use this for initialization
+    void Start()
+    {
+        relations = new Dictionary<Social, float>();
+        opinions = new Dictionary<string, float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateRelations()
+    {
+        // l ö ö p sur toutes les relations
+        // update opinion selon force de la relation + rnd 
+        float totalRelationForces = 0;
+        foreach (KeyValuePair<Social, float> relation in relations)
+        {
+            totalRelationForces += relation.Value;
+        }
+
+        foreach (KeyValuePair<string, float> opinion in opinions)
+        {
+            foreach(KeyValuePair<Social, float> relation in relations)
+            {
+                opinions[opinion.Key] += relation.Value * relation.Key.opinions[opinion.Key] / totalRelationForces;
+            }
+        }
+    }
+}
---
+++ ThatOneThing/Assets/Scripts/SocialImpact.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SocialImpact : Impact
+{
+    [SerializeField]
+    string opinion;
+
+    [SerializeField]
+    Social relation;
+
+    [SerializeField]
+    float deltaOpinion, deltaRelation;
+
+    private Social socialComponent;
+
+    public override void Activate()
+    {
+        socialComponent.opinions[opinion] += deltaOpinion;
+    }
+    
+}
