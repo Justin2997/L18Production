9117c7c4478d048fc460a1f8406ff97c21371a8e
Charles <charles.murphy.042@gmail.com>
2019-01-26 18:37:51 -0500

    tweaks trigger + dialogue

--- ThatOneThing/Assets/Scripts/WellBeingManager.cs
+++ ThatOneThing/Assets/Scripts/WellBeingManager.cs
@@ -41,7 +41,6 @@ public class WellBeingManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;
     }
 
     // Update is called once per frame
