7988f600f2400caba80c5f316f18a5db3980ab32
Justin Brûlotte <Justin2997@users.noreply.github.com>
2019-01-26 19:47:56 -0500

    Charles (#15)
    * Added some sprites and tilepalettes
    
    * First draft scripts
    
    * debut de la map
    
    * Well Being stuff - v1
    
    slider + meter dans le UI
    
    * cleanup machins
    
    * MODIF GITignore
    
    * First draft scripts
    
    * Well Being stuff - v1
    
    slider + meter dans le UI
    
    * MODIF GITignore
    
    * social stuff v# + ignore .info
    
    * Adding moving
    
    * Prefabs
    
    * fuck library
    
    * Point of Interests triggers + dialogues
    
    * tweaks trigger + dialogue
    
    * Adapt pour anim

--- ThatOneThing/Assets/Prefab/Dialogue/DialogueSystem.cs
+++ ThatOneThing/Assets/Prefab/Dialogue/DialogueSystem.cs
@@ -38,7 +38,6 @@ public class DialogueSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;
     }
 
     // Update is called once per frame
