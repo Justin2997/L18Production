b491735ac49d9dae35d810e38290405d9985dc44
PlasticPack <plasticpack@hotmail.ca>
2019-01-27 10:36:47 -0500

    Animation personnage

--- ThatOneThing/Assets/Thomas/scripts/move.cs
+++ ThatOneThing/Assets/Thomas/scripts/move.cs
@@ -21,39 +21,36 @@ public class move : MonoBehaviour
             rb.AddForce(Vector2.up * speed);
             ismoving = true;
         }
-        else
-        {
-            ismoving = false;
-        }
-
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(-Vector2.right * speed);
             ismoving = true;
         }
-        else
-        {
-            ismoving = false;
-        }
-
         if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(-Vector2.up * speed);
             ismoving = true;
         }
-        else
-        {
-            ismoving = false;
-        }
-
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(Vector2.right * speed);
             ismoving = true;
         }
+
+        Animator animator = GetComponent<Animator>();
+
+        ismoving = Mathf.Abs(rb.velocity.x) > 0.1 || Mathf.Abs(rb.velocity.x) > 0.1;
+        animator.SetBool("IsMoving", ismoving);
+
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+        {
+            animator.SetFloat("xVelocity", rb.velocity.x);
+            animator.SetFloat("yVelocity", 0);
+        }
         else
         {
-            ismoving = false;
+            animator.SetFloat("yVelocity", rb.velocity.y);
+            animator.SetFloat("xVelocity", 0);
         }
     }
 }
